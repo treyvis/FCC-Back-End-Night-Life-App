778e768d9ec0046db28c7f3a21cc97b1d520f279
Trey <treyvis@gmail.com>
2017-10-27 08:10:20 -0600

    Making Main.js modular

--- src/js/App.js
+++ src/js/App.js
@@ -6,17 +6,8 @@ import { Provider } from 'react-redux';
 import { initialize } from './actions/session';
 import store from './store';
 
-import Header from 'grommet/components/Header';
-import Title from 'grommet/components/Title';
-import Box from 'grommet/components/Box';
-import Menu from 'grommet/components/Menu';
-import Anchor from 'grommet/components/Anchor';
 import App from 'grommet/components/App';
-import Actions from 'grommet/components/icons/base/Actions';
-import Card from 'grommet/components/Card';
-import Tiles from 'grommet/components/Tiles';
-import Tile from 'grommet/components/Tile';
-import Search from 'grommet/components/Search';
+import Main from './pages/Main.js';
 
 
 const locale = getCurrentLocale();
@@ -37,52 +28,7 @@ export default () => (
   <Provider store={store}>
     <IntlProvider locale={localeData.locale} messages={localeData.messages}>
       <App>
-        <Header>
-          <Title>
-            Night Life App | Salt Lake City
-          </Title>
-          <Box flex={true}
-            justify='end'
-            direction='row'
-            responsive={false}>
-            <Search inline={true}
-              fill={true}
-              size={'medium'}
-              placeHolder={'Search'}
-              suggestions={['Salt Lake City','San Francisco', 'Seatle', 'Chicago']}
-              dropAlign={{"top": "bottom"}} />
-            <Menu icon={<Actions />}
-              dropAlign={{ right: 'right' }}>
-              <Anchor href='#'
-                className='active'>
-                Sign Up
-              </Anchor>
-              <Anchor href='#'>
-                Login
-              </Anchor>
-              <Anchor href='#'>
-                Logout
-              </Anchor>
-            </Menu>
-          </Box>
-        </Header>
-        <Tiles flush>
-          <Tile>
-            <Card thumbnail='/img/dan-gold-105699.jpg'
-              heading='Burger 13'
-              description='Delectable burgers with an artistic touch.' />
-          </Tile>
-          <Tile>
-            <Card thumbnail='/img/dan-gold-105699.jpg'
-              heading='Burger 13'
-              description='Delectable burgers with an artistic touch.' />
-          </Tile>
-          <Tile>
-            <Card thumbnail='/img/dan-gold-105699.jpg'
-              heading='Burger 13'
-              description='Delectable burgers with an artistic touch.' />
-          </Tile>
-        </Tiles>
+        <Main />
       </App>
     </IntlProvider>
   </Provider>
