277de27695733ef6948ff95fa4340103b1334c01
Trey <treyvis@gmail.com>
2017-10-27 08:34:14 -0600

    Adding react router and test signup and login pages

--- src/js/App.js
+++ src/js/App.js
@@ -5,9 +5,12 @@ import { getCurrentLocale, getLocaleData } from 'grommet/utils/Locale';
 import { Provider } from 'react-redux';
 import { initialize } from './actions/session';
 import store from './store';
+import { BrowserRouter, Switch, Route, Link } from 'react-router-dom'
 
 import App from 'grommet/components/App';
 import Main from './pages/Main.js';
+import Login from './pages/Login.js';
+import SignUp from './pages/SignUp.js';
 
 
 const locale = getCurrentLocale();
@@ -25,11 +28,17 @@ if (window.location.pathname !== '/login') {
 }
 
 export default () => (
-  <Provider store={store}>
-    <IntlProvider locale={localeData.locale} messages={localeData.messages}>
-      <App>
-        <Main />
-      </App>
-    </IntlProvider>
-  </Provider>
+  <BrowserRouter>
+    <Provider store={store}>
+      <IntlProvider locale={localeData.locale} messages={localeData.messages}>
+        <App>
+          <Switch>
+            <Route exact path='/' component={Main} />
+            <Route path='/login' component={Login} />
+            <Route path='/signup' component={SignUp} />
+          </Switch>
+        </App>
+      </IntlProvider>
+    </Provider>
+  </BrowserRouter>
 );
